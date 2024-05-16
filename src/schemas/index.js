1a3e368cdcf4a717145f88b138b244e2892cb8ae
Parkseojin2 <bym401543@gmail.com>
2024-05-17 03:46:02 +0900

    몽고디비 수정

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 const connect = () => {
   mongoose
-    .connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true, useUnifiedTopology: true
-    })
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log('몽고디비 연결 성공'))
     .catch((err) => console.error('몽고디비 연결 실패:', err));
 };
@@ -17,3 +15,26 @@ mongoose.connection.on('error', (err) => {
 });
 
 export default connect;
+
+
+// // /schemas/index.js
+
+// import mongoose from 'mongoose';
+
+// const connect = () => {
+//   mongoose
+//     .connect(
+//       'mongodb+srv://sparta-user:aaaa4321@express-mongo.uy7ttg7.mongodb.net/?retryWrites=true&w=majority',
+//       {
+//         dbName: 'spa_mall', // spa_mall 데이터베이스명을 사용합니다.
+//       },
+//     )
+//     .catch((err) => console.log(err))
+//     .then(() => console.log('몽고디비 연결 성공'));
+// };
+
+// mongoose.connection.on('error', (err) => {
+//   console.error('몽고디비 연결 에러', err);
+// });
+
+// export default connect;
